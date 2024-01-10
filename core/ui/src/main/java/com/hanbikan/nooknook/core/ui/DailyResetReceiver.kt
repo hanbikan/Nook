334528843d2da2b83b1f915b0c8db7905c87eabc
hanbikan <fchopinof99@gmail.com>
2024-01-10 21:05:57 +0900

    Implement DailyResetScheduler

--- core/ui/src/main/java/com/hanbikan/nooknook/core/ui/DailyResetReceiver.kt
+++ core/ui/src/main/java/com/hanbikan/nooknook/core/ui/DailyResetReceiver.kt
@@ -9,8 +9,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-// 매일 일정 시각에 호출됩니다. 기본적으로 새벽 5시에 호출되며, 타임슬립 유저를 위한 시각 변경을 고려해야 합니다.
-class DailyAlarmReceiver @Inject constructor(
+class DailyResetReceiver @Inject constructor(
     private val resetAllTasksUseCase: ResetAllTasksUseCase
 ): BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
---
+++ core/ui/src/main/java/com/hanbikan/nooknook/core/ui/DailyResetScheduler.kt
@@ -0,0 +1,41 @@
+package com.hanbikan.nooknook.core.ui
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import java.util.Calendar
+
+/**
+ * 매일 오전 5시에 [DailyResetReceiver]를 호출합니다.
+ */
+class DailyResetScheduler {
+
+    companion object {
+        fun scheduleDailyAlarm(context: Context) {
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val intent = Intent(context, DailyResetReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+
+            // TODO: 타임슬립 유저를 위한 데일리 리셋 시간 변경
+            val calendar = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 5)
+                set(Calendar.MINUTE, 0)
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+
+                // 현재 시간이 이미 데일리 리셋 시간을 지났다면 다음 날로 설정
+                if (after(Calendar.getInstance())) {
+                    add(Calendar.DATE, 1)
+                }
+            }
+
+            alarmManager.setRepeating(
+                AlarmManager.ELAPSED_REALTIME,
+                calendar.timeInMillis,
+                AlarmManager.INTERVAL_DAY,
+                pendingIntent
+            )
+        }
+    }
+}
