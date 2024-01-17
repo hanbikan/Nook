c55fd6baf3d5ede7bf896bf4087f029e2b096ad1
hanbikan <fchopinof99@gmail.com>
2024-01-17 14:36:06 +0900

    Implement DI and database for user

--- core/database/src/main/java/com/hanbikan/nooknook/core/database/NnDatabase.kt
+++ core/database/src/main/java/com/hanbikan/nooknook/core/database/NnDatabase.kt
@@ -3,9 +3,11 @@ package com.hanbikan.nooknook.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.hanbikan.nooknook.core.database.dao.TaskDao
+import com.hanbikan.nooknook.core.database.dao.UserDao
 import com.hanbikan.nooknook.core.database.entity.TaskEntity
 
 @Database(entities = [TaskEntity::class], version = 1)
 abstract class NnDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
+    abstract fun userDao(): UserDao
 }
--- core/database/src/main/java/com/hanbikan/nooknook/core/database/di/DaoModule.kt
+++ core/database/src/main/java/com/hanbikan/nooknook/core/database/di/DaoModule.kt
@@ -2,6 +2,7 @@ package com.hanbikan.nooknook.core.database.di
 
 import com.hanbikan.nooknook.core.database.NnDatabase
 import com.hanbikan.nooknook.core.database.dao.TaskDao
+import com.hanbikan.nooknook.core.database.dao.UserDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,6 +14,11 @@ object DaoModule {
 
     @Provides
     fun providesTaskDao(
-        database: NnDatabase
-    ) : TaskDao = database.taskDao()
+        database: NnDatabase,
+    ): TaskDao = database.taskDao()
+
+    @Provides
+    fun providesUserDao(
+        database: NnDatabase,
+    ): UserDao = database.userDao()
 }
