134f5dbb26b72258eb8bd7533dd6637f2835948f
hanbikan <fchopinof99@gmail.com>
2024-01-18 20:54:27 +0900

    Implement AddUserViewModel

---
+++ feature/tutorial/src/main/java/com/hanbikan/nooknook/feature/tutorial/AddUserViewModel.kt
@@ -0,0 +1,40 @@
+package com.hanbikan.nooknook.feature.tutorial
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hanbikan.nooknook.core.domain.model.User
+import com.hanbikan.nooknook.core.domain.usecase.AddUserUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddUserViewModel @Inject constructor(
+    private val addUserUseCase: AddUserUseCase,
+) : ViewModel() {
+
+    private val _name: MutableStateFlow<String> = MutableStateFlow("")
+    val name = _name.asStateFlow()
+
+    private val _islandName: MutableStateFlow<String> = MutableStateFlow("")
+    val islandName = _islandName.asStateFlow()
+
+    fun setName(newName: String) {
+        _name.value = newName
+    }
+
+    fun setIslandName(newIslandName: String) {
+        _islandName.value = newIslandName
+    }
+
+    fun addUser(onComplete: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val user = User(0, name.value, islandName.value)
+            addUserUseCase(user)
+            onComplete()
+        }
+    }
+}
