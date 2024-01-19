e310145bd4f2759d0e4fea45859eb74f4885ecff
hanbikan <fchopinof99@gmail.com>
2024-01-19 15:33:37 +0900

    Implement writing datastore

--- core/datastore/src/main/java/com/hanbikan/nooknook/core/datastore/NnDataStore.kt
+++ core/datastore/src/main/java/com/hanbikan/nooknook/core/datastore/NnDataStore.kt
@@ -3,6 +3,7 @@ package com.hanbikan.nooknook.core.datastore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
@@ -12,9 +13,15 @@ import javax.inject.Inject
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore("preferences")
 
 class NnDataStore @Inject constructor(
-    context: Context
+    private val context: Context
 ) {
     val activeUserIdFlow: Flow<Int?> = context.dataStore.data.map { it[ACTIVE_USER_ID] }
+
+    suspend fun setActiveUserId(id: Int) {
+        context.dataStore.edit {
+            it[ACTIVE_USER_ID] = id
+        }
+    }
 
     companion object {
         val ACTIVE_USER_ID = intPreferencesKey("active_user_id")
