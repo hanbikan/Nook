a996749525a14ffd5d9522de47f33f9afcd7f724
hanbikan <fchopinof99@gmail.com>
2024-01-25 18:07:15 +0900

    Modify delaying splash logic

--- app/src/main/java/com/hanbikan/nook/MainActivity.kt
+++ app/src/main/java/com/hanbikan/nook/MainActivity.kt
@@ -1,30 +1,17 @@
 package com.hanbikan.nook
 
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
-import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.core.animation.doOnEnd
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hanbikan.nook.core.designsystem.theme.NkTheme
-import com.hanbikan.nook.core.domain.usecase.GetLastVisitedRouteUseCase
 import com.hanbikan.nook.feature.tutorial.navigation.welcomeScreenRoute
 import com.hanbikan.nook.ui.NkApp
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -35,37 +22,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 스플래시를 지연시키고, 그동안 GetLastVisitedRouteUseCase를 수행하여 자연스러운 화면을 유도합니다.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                ObjectAnimator.ofFloat(
-                    splashScreenView,
-                    View.ALPHA,
-                    1f,
-                    0f
-                ).apply {
-                    duration = 300L
-                    doOnEnd { splashScreenView.remove() }
-                    start()
-                }
-            }
-        }
-
         setContent {
             NkTheme {
-                val isReady = viewModel.isReady.collectAsStateWithLifecycle().value
                 val lastVisitedRoute = viewModel.lastVisitedRoute.collectAsStateWithLifecycle().value
-
-                if (!isReady) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(NkTheme.colorScheme.background)
-                    ) {}
-                } else {
-                    NkApp(startDestination = lastVisitedRoute ?: welcomeScreenRoute)
-                }
+                NkApp(startDestination = lastVisitedRoute ?: welcomeScreenRoute)
             }
         }
+
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check whether the initial data is ready.
+                    return if (viewModel.isReady.value) {
+                        // The content is ready. Start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content isn't ready. Suspend.
+                        false
+                    }
+                }
+            }
+        )
     }
 }
