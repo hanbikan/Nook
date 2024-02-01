3a676cfd18d3c973f0e2e65c8485c845fd6e69f8
hanbikan <fchopinof99@gmail.com>
2024-02-01 10:56:13 +0900

    Add TutorialTaskEntity

---
+++ core/database/src/main/java/com/hanbikan/nook/core/database/entity/TutorialTaskEntity.kt
@@ -0,0 +1,14 @@
+package com.hanbikan.nook.core.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tutorial_task")
+data class TutorialTaskEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "user_id", defaultValue = "-1") val userId: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "is_done") val isDone: Boolean,
+    @ColumnInfo(name = "day") val day: Int
+)
