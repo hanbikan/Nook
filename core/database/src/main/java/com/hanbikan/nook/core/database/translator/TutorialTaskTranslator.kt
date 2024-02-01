00569861fd266098e2a3208da0a999b8b50d481f
hanbikan <fchopinof99@gmail.com>
2024-02-01 10:56:47 +0900

    Implement TutorialTaskTranslator

---
+++ core/database/src/main/java/com/hanbikan/nook/core/database/translator/TutorialTaskTranslator.kt
@@ -0,0 +1,12 @@
+package com.hanbikan.nook.core.database.translator
+
+import com.hanbikan.nook.core.database.entity.TutorialTaskEntity
+import com.hanbikan.nook.core.domain.model.TutorialTask
+
+fun TutorialTaskEntity.toDomain(): TutorialTask {
+    return TutorialTask(id, userId, name, isDone, day)
+}
+
+fun TutorialTask.toData(): TutorialTaskEntity {
+    return TutorialTaskEntity(id, userId, name, isDone, day)
+}
