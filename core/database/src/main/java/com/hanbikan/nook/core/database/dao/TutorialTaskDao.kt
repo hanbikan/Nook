2e79b7d1735105907bc62a7112bb7b1648271e94
hanbikan <fchopinof99@gmail.com>
2024-02-01 10:56:20 +0900

    Add TutorialTaskDao

---
+++ core/database/src/main/java/com/hanbikan/nook/core/database/dao/TutorialTaskDao.kt
@@ -0,0 +1,20 @@
+package com.hanbikan.nook.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.hanbikan.nook.core.database.entity.TutorialTaskEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TutorialTaskDao {
+    @Query("SELECT * FROM tutorial_task WHERE tutorial_task.user_id == :userId and tutorial_task.user_id == :day")
+    fun getTasksByUserIdAndDay(userId: Int, day: Int): Flow<List<TutorialTaskEntity>>
+
+    @Insert
+    suspend fun insertTutorialTasks(vararg tutorialTaskEntities: TutorialTaskEntity)
+
+    @Update
+    suspend fun updateTutorialTask(tutorialTaskEntity: TutorialTaskEntity)
+}
