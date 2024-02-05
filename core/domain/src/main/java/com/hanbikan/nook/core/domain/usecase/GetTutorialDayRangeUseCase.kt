58b48a6f13243a5a611a5d2158e6e5887aa2a3e0
hanbikan <fchopinof99@gmail.com>
2024-02-05 20:33:54 +0900

    Modify GetTutorialDayRangeUseCase to return flow

--- core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/GetTutorialDayRangeUseCase.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/GetTutorialDayRangeUseCase.kt
@@ -1,7 +1,8 @@
 package com.hanbikan.nook.core.domain.usecase
 
 import com.hanbikan.nook.core.domain.repository.TutorialTaskRepository
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 import kotlin.math.max
 import kotlin.math.min
@@ -9,14 +10,15 @@ import kotlin.math.min
 class GetTutorialDayRangeUseCase @Inject constructor(
     private val tutorialTaskRepository: TutorialTaskRepository
 ) {
-    suspend operator fun invoke(userId: Int): IntRange? {
-        val tutorialTasks = tutorialTaskRepository.getTutorialTasksByUserId(userId)
-        var minTutorialDay: Int? = null
-        var maxTutorialDay: Int? = null
-        tutorialTasks.first().forEach {
-            minTutorialDay = min(minTutorialDay ?: Int.MAX_VALUE, it.day) // null이면 it.day 대입
-            maxTutorialDay = max(maxTutorialDay ?: 0, it.day) // null이면 it.day 대입
+    operator fun invoke(userId: Int): Flow<IntRange?> {
+        return tutorialTaskRepository.getTutorialTasksByUserId(userId).mapLatest { tutorialTasks ->
+            var minTutorialDay: Int? = null
+            var maxTutorialDay: Int? = null
+            tutorialTasks.forEach {
+                minTutorialDay = min(minTutorialDay ?: Int.MAX_VALUE, it.day) // null이면 it.day 대입
+                maxTutorialDay = max(maxTutorialDay ?: 0, it.day) // null이면 it.day 대입
+            }
+            maxTutorialDay?.let { minTutorialDay?.rangeTo(it) }
         }
-        return maxTutorialDay?.let { minTutorialDay?.rangeTo(it) }
     }
 }
