99f518985d83ce86d6026bf6766090569152be58
hanbikan <fchopinof99@gmail.com>
2024-02-05 20:31:21 +0900

    Modify GetMaxTutorialDayUseCase to GetTutorialDayRangeUseCase

--- core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/GetTutorialDayRangeUseCase.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/GetTutorialDayRangeUseCase.kt
@@ -4,16 +4,19 @@ import com.hanbikan.nook.core.domain.repository.TutorialTaskRepository
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 import kotlin.math.max
+import kotlin.math.min
 
-class GetMaxTutorialDayUseCase @Inject constructor(
+class GetTutorialDayRangeUseCase @Inject constructor(
     private val tutorialTaskRepository: TutorialTaskRepository
 ) {
-    suspend operator fun invoke(userId: Int): Int? {
+    suspend operator fun invoke(userId: Int): IntRange? {
         val tutorialTasks = tutorialTaskRepository.getTutorialTasksByUserId(userId)
+        var minTutorialDay: Int? = null
         var maxTutorialDay: Int? = null
         tutorialTasks.first().forEach {
-            maxTutorialDay = max(maxTutorialDay ?: 0, it.day) // elvis를 넣은 이유는 null일 때는 day를 그대로 넣기 위함입니다.(0 + day)
+            minTutorialDay = min(minTutorialDay ?: Int.MAX_VALUE, it.day) // null이면 it.day 대입
+            maxTutorialDay = max(maxTutorialDay ?: 0, it.day) // null이면 it.day 대입
         }
-        return maxTutorialDay
+        return maxTutorialDay?.let { minTutorialDay?.rangeTo(it) }
     }
 }
