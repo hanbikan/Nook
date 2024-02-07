6a2a517ec5c5181770d0d2695647a781f07cb808
hanbikan <fchopinof99@gmail.com>
2024-02-07 17:27:14 +0900

    Implement NkInfoButton

---
+++ core/designsystem/src/main/java/com/hanbikan/nook/core/designsystem/component/NkInfoButton.kt
@@ -0,0 +1,46 @@
+package com.hanbikan.nook.core.designsystem.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.hanbikan.nook.core.designsystem.theme.Dimens
+import com.hanbikan.nook.core.designsystem.theme.NkTheme
+
+@Composable
+fun NkInfoButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = NkTheme.colorScheme.primaryContainer,
+) {
+    val interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+    Icon(
+        modifier = modifier
+            .size(Dimens.IconExtraSmall)
+            .clickable(
+                onClick = onClick,
+                interactionSource = interactionSource,
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = Dimens.IconExtraSmall
+                ),
+            ),
+        imageVector = Icons.Default.Info,
+        contentDescription = null,
+        tint = tint
+    )
+}
+
+@Composable
+@Preview
+fun NkInfoButtonPreview() {
+    NkInfoButton({})
+}
