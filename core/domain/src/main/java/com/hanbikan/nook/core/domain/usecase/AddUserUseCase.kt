bd7cd9b3e05657239a31972345dd817feccba253
hanbikan <fchopinof99@gmail.com>
2024-02-08 19:48:27 +0900

    Remove unnecessary code

--- core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/AddUserUseCase.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/AddUserUseCase.kt
@@ -3,19 +3,16 @@ package com.hanbikan.nook.core.domain.usecase
 import com.hanbikan.nook.core.domain.model.User
 import com.hanbikan.nook.core.domain.repository.AppStateRepository
 import com.hanbikan.nook.core.domain.repository.UserRepository
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class AddUserUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val appStateRepository: AppStateRepository,
-    private val initializeTutorialTasksUseCase: InitializeTutorialTasksUseCase,
 ) {
     suspend operator fun invoke(user: User) {
         val addedUserId = userRepository.insertUser(user)
 
         // 후속 작업
         appStateRepository.setActiveUserId(addedUserId)
-        initializeTutorialTasksUseCase(addedUserId)
     }
 }
