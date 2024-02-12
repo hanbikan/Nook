7bf8830e3ed149e71fac98af09f65a67e059f5bf
hanbikan <fchopinof99@gmail.com>
2024-02-12 20:56:34 +0900

    Add NkText with inlineContent

--- core/designsystem/src/main/java/com/hanbikan/nook/core/designsystem/component/NkText.kt
+++ core/designsystem/src/main/java/com/hanbikan/nook/core/designsystem/component/NkText.kt
@@ -1,12 +1,15 @@
 package com.hanbikan.nook.core.designsystem.component
 
+import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
@@ -20,6 +23,7 @@ fun NkText(
     modifier: Modifier = Modifier,
     color: Color = NkTheme.colorScheme.primary,
     fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = Fonts.joa,
     textDecoration: TextDecoration? = null,
     textAlign: TextAlign? = null,
     overflow: TextOverflow = TextOverflow.Ellipsis,
@@ -32,12 +36,43 @@ fun NkText(
         modifier = modifier,
         color = color,
         fontWeight = fontWeight,
-        fontFamily = Fonts.joa,
+        fontFamily = fontFamily,
         textDecoration = textDecoration,
         textAlign = textAlign,
         overflow = overflow,
         maxLines = maxLines,
         onTextLayout = onTextLayout,
         style = style
+    )
+}
+
+@Composable
+fun NkText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    color: Color = NkTheme.colorScheme.primary,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = Fonts.joa,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    overflow: TextOverflow = TextOverflow.Ellipsis,
+    maxLines: Int = Int.MAX_VALUE,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        overflow = overflow,
+        maxLines = maxLines,
+        inlineContent = inlineContent,
+        onTextLayout = onTextLayout,
+        style = style,
     )
 }
