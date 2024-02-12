d3bbb8e77fd302d6adabbe06562a8f6536be224f
hanbikan <fchopinof99@gmail.com>
2024-02-12 20:57:04 +0900

    Fix dark mode status bar

--- core/designsystem/src/main/java/com/hanbikan/nook/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/hanbikan/nook/core/designsystem/theme/Theme.kt
@@ -59,7 +59,7 @@ fun NkTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
