aa1f4f2b672acf14864958a367524e3146375b17
hanbikan <fchopinof99@gmail.com>
2024-02-21 18:15:15 +0900

    Add top app bar in profile screen

--- feature/profile/src/main/java/com/hanbikan/nook/feature/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/hanbikan/nook/feature/profile/ProfileScreen.kt
@@ -1,19 +1,42 @@
 package com.hanbikan.nook.feature.profile
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.hanbikan.nook.core.designsystem.component.NkText
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.hanbikan.nook.core.designsystem.component.AppBarIcon
+import com.hanbikan.nook.core.designsystem.component.NkTopAppBar
+import com.hanbikan.nook.core.ui.UserDialog
 
 @Composable
 fun ProfileScreen(
     navigateToAddUser: () -> Unit,
     navigateToPhone: () -> Unit,
+    viewModel: ProfileViewModel = hiltViewModel(),
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        NkText("Profile Screen")
+    val isUserDialogShown = viewModel.isUserDialogShown.collectAsStateWithLifecycle().value
+
+    Box {
+        Column(modifier = Modifier.fillMaxSize()) {
+            NkTopAppBar(
+                leftAppBarIcons = listOf(
+                    AppBarIcon.appListAppBarIcon(onClick = navigateToPhone)
+                ),
+                rightAppBarIcons = listOf(
+                    AppBarIcon.userDialogAppBarIcon(onClick = viewModel::switchUserDialog)
+                ),
+            )
+
+            // TODO
+        }
+
+        UserDialog(
+            visible = isUserDialogShown,
+            navigateToAddUser = navigateToAddUser,
+            onDismissRequest = viewModel::switchUserDialog
+        )
     }
 }
---
+++ feature/profile/src/main/java/com/hanbikan/nook/feature/profile/ProfileViewModel.kt
@@ -0,0 +1,20 @@
+package com.hanbikan.nook.feature.profile
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+
+) : ViewModel() {
+    // Dialog
+    private val _isUserDialogShown: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isUserDialogShown = _isUserDialogShown.asStateFlow()
+
+    fun switchUserDialog() {
+        _isUserDialogShown.value = !isUserDialogShown.value
+    }
+}
