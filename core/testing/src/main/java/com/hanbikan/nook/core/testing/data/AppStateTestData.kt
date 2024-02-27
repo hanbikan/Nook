930869c0e3a743efe4710a0b96a2a90bba5f2a24
hanbikan <fchopinof99@gmail.com>
2024-02-27 15:01:46 +0900

    Add AppStateTestData

---
+++ core/testing/src/main/java/com/hanbikan/nook/core/testing/data/AppStateTestData.kt
@@ -0,0 +1,6 @@
+package com.hanbikan.nook.core.testing.data
+
+import com.hanbikan.nook.feature.todo.navigation.todoScreenRoute
+
+var activeUserIdTestData: Int = userTestData[0].id
+var lastVisitedRouteTestData: String = todoScreenRoute
