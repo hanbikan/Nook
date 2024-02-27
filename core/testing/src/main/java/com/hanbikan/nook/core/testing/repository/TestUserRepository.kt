b8a636926e2f5a6101b2f9bc94675fa838600146
hanbikan <fchopinof99@gmail.com>
2024-02-27 14:54:38 +0900

    Implement TestUserRepository

---
+++ core/testing/src/main/java/com/hanbikan/nook/core/testing/repository/TestUserRepository.kt
@@ -0,0 +1,35 @@
+package com.hanbikan.nook.core.testing.repository
+
+import com.hanbikan.nook.core.domain.model.User
+import com.hanbikan.nook.core.domain.repository.UserRepository
+import com.hanbikan.nook.core.testing.data.userTestData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class TestUserRepository : UserRepository {
+    override fun getAllUsers(): Flow<List<User>> {
+        return flowOf(userTestData)
+    }
+
+    override fun getUserById(id: Int): Flow<User?> {
+        val user = userTestData.find { it.id == id }
+        return flowOf(user)
+    }
+
+    override suspend fun insertUser(user: User): Int {
+        val addedUserId = if (userTestData.isNotEmpty()) {
+            userTestData.last().id + 1
+        } else {
+            0
+        }
+        val newUserTestData = userTestData.toMutableList().apply {
+            add(user.copy(id = addedUserId))
+        }
+        userTestData = newUserTestData
+        return addedUserId
+    }
+
+    override suspend fun deleteUserById(id: Int) {
+        userTestData = userTestData.filter { it.id != id }
+    }
+}
