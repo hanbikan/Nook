0500c4cbe55dc0aef7b0b4e9fd3ed8e12f0fe3b6
hanbikan <fchopinof99@gmail.com>
2024-02-27 15:20:15 +0900

    Implement TestTaskRepository

---
+++ core/testing/src/main/java/com/hanbikan/nook/core/testing/data/TaskTestData.kt
@@ -0,0 +1,5 @@
+package com.hanbikan.nook.core.testing.data
+
+import com.hanbikan.nook.core.domain.model.Task
+
+var tasksTestData: List<Task> = listOf()
---
+++ core/testing/src/main/java/com/hanbikan/nook/core/testing/repository/TestTaskRepository.kt
@@ -0,0 +1,37 @@
+package com.hanbikan.nook.core.testing.repository
+
+import com.hanbikan.nook.core.domain.model.Task
+import com.hanbikan.nook.core.domain.repository.TaskRepository
+import com.hanbikan.nook.core.testing.data.tasksTestData
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class TestTaskRepository : TaskRepository {
+    override fun getAllTasksByUserId(userId: Int): Flow<List<Task>> {
+        val tasks = tasksTestData.filter { it.userId == userId }
+        return flowOf(tasks)
+    }
+
+    override suspend fun insertTasks(taskList: List<Task>) {
+        tasksTestData = tasksTestData + taskList
+    }
+
+    override suspend fun updateTask(task: Task) {
+        val indexToUpdate = tasksTestData.indexOfFirst { it.id == task.id }
+        if (indexToUpdate == -1) return
+
+        val newTasks = tasksTestData.toMutableList().apply {
+            this[indexToUpdate] = task
+        }
+        tasksTestData = newTasks
+    }
+
+    override suspend fun deleteTaskById(id: Int) {
+        val newTasks = tasksTestData.filter { it.id != id }
+        tasksTestData = newTasks
+    }
+
+    override suspend fun resetAllDailyTasks() {
+        tasksTestData = listOf()
+    }
+}
