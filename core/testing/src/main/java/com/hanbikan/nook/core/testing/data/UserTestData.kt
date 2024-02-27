f6c7f053ed25bdc0168f15cca74b3b66b07635ee
hanbikan <fchopinof99@gmail.com>
2024-02-27 14:55:00 +0900

    Add userTestData

---
+++ core/testing/src/main/java/com/hanbikan/nook/core/testing/data/UserTestData.kt
@@ -0,0 +1,30 @@
+package com.hanbikan.nook.core.testing.data
+
+import com.hanbikan.nook.core.domain.model.User
+
+var userTestData: List<User> = listOf(
+    User(
+        id = 0,
+        name = "user0",
+        islandName = "island0",
+        tutorialDay = 0,
+    ),
+    User(
+        id = 1,
+        name = "user1",
+        islandName = "island1",
+        tutorialDay = 1,
+    ),
+    User(
+        id = 2,
+        name = "user2",
+        islandName = "island2",
+        tutorialDay = 2,
+    ),
+    User(
+        id = 3,
+        name = "user3",
+        islandName = "island3",
+        tutorialDay = 3,
+    ),
+)
