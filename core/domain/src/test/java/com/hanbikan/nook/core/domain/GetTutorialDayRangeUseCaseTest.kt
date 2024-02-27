67b75e56a5439e5ca9fd5aa2b4ae1887028fd0af
hanbikan <fchopinof99@gmail.com>
2024-02-27 18:13:00 +0900

    Implement GetTutorialDayRangeUseCaseTest

---
+++ core/domain/src/test/java/com/hanbikan/nook/core/domain/GetTutorialDayRangeUseCaseTest.kt
@@ -0,0 +1,28 @@
+package com.hanbikan.nook.core.domain
+
+import com.hanbikan.nook.core.domain.repository.testTutorialTaskRepository
+import com.hanbikan.nook.core.domain.usecase.GetTutorialDayRangeUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GetTutorialDayRangeUseCaseTest {
+
+    private val useCase = GetTutorialDayRangeUseCase(
+        tutorialTaskRepository = testTutorialTaskRepository
+    )
+
+    @Test
+    fun `verify day range is correct for given user ID`() = runTest {
+        val testUserId = 0
+        val result = useCase(testUserId).first()
+
+        val tutorialTasks = testTutorialTaskRepository.getTutorialTasksByUserId(testUserId).first()
+            .filter { it.userId == testUserId }
+        val minDay = tutorialTasks.minOf { it.day }
+        val maxDay = tutorialTasks.maxOf { it.day }
+
+        assertEquals(result, minDay.rangeTo(maxDay))
+    }
+}
