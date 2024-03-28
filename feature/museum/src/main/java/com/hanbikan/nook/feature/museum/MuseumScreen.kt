20c2e23e33418fd188b452054fbc6a503cc0828a
hanbikan <fchopinof99@gmail.com>
2024-03-28 17:58:11 +0900

    Add MuseumScreen

---
+++ feature/museum/src/main/java/com/hanbikan/nook/feature/museum/MuseumScreen.kt
@@ -0,0 +1,13 @@
+package com.hanbikan.nook.feature.museum
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun MuseumScreen(
+    navigateToAddUser: () -> Unit,
+    navigateToPhone: () -> Unit,
+    viewModel: MuseumViewModel = hiltViewModel(),
+) {
+
+}
