49ca91009cc7502e16dd7332a6c55dfc3ab325a9
hanbikan <fchopinof99@gmail.com>
2024-03-28 17:58:19 +0900

    Add MuseumViewModel

---
+++ feature/museum/src/main/java/com/hanbikan/nook/feature/museum/MuseumViewModel.kt
@@ -0,0 +1,11 @@
+package com.hanbikan.nook.feature.museum
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MuseumViewModel @Inject constructor(
+
+) : ViewModel() {
+}
