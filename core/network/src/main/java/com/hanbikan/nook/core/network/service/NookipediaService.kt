b44b1ec1488e6e323064983194a545cd48b27da9
hanbikan <fchopinof99@gmail.com>
2024-03-29 18:05:25 +0900

    Fix NookipediaService

--- core/network/src/main/java/com/hanbikan/nook/core/network/service/NookipediaService.kt
+++ core/network/src/main/java/com/hanbikan/nook/core/network/service/NookipediaService.kt
@@ -1,11 +1,10 @@
 package com.hanbikan.nook.core.network.service
 
 import com.hanbikan.nook.core.network.response.FishResponse
-import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 
 interface NookipediaService {
 
     @GET("/nh/fish")
-    fun getAllFishes(): Flow<List<FishResponse>>
+    suspend fun getAllFishes(): List<FishResponse>
 }
