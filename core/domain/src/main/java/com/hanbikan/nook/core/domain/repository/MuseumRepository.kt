74fa83a384abb9cad8535a2e990465515587691a
hanbikan <fchopinof99@gmail.com>
2024-03-29 17:09:39 +0900

    Add MuseumRepository

---
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/repository/MuseumRepository.kt
@@ -0,0 +1,11 @@
+package com.hanbikan.nook.core.domain.repository
+
+import com.hanbikan.nook.core.domain.model.Bug
+import com.hanbikan.nook.core.domain.model.Fish
+import com.hanbikan.nook.core.domain.model.SeaCreature
+
+interface MuseumRepository {
+    fun getAllFishes(): List<Fish>
+    fun getAllBugs(): List<Bug>
+    fun getAllSeaCreatures(): List<SeaCreature>
+}
