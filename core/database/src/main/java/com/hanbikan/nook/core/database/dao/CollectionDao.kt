50da7ff2a9c63afc21befba5f01818e60dba0b9d
hanbikan <fchopinof99@gmail.com>
2024-03-30 15:44:45 +0900

    Implement CollectionDao

---
+++ core/database/src/main/java/com/hanbikan/nook/core/database/dao/CollectionDao.kt
@@ -0,0 +1,20 @@
+package com.hanbikan.nook.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.hanbikan.nook.core.database.entity.FishEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CollectionDao {
+    @Query("SELECT * FROM fish WHERE fish.user_id == :userId ORDER BY number")
+    fun getAllFishesByUserId(userId: Int): Flow<List<FishEntity>>
+
+    @Insert
+    suspend fun insertFishes(vararg fishEntities: FishEntity)
+
+    @Update
+    suspend fun updateFish(fishEntity: FishEntity)
+}
--- core/database/src/main/java/com/hanbikan/nook/core/database/entity/FishEntity.kt
+++ core/database/src/main/java/com/hanbikan/nook/core/database/entity/FishEntity.kt
@@ -4,9 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "user")
+@Entity(tableName = "fish")
 data class FishEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "user_id") val userId: Int,
     val name: String,
     val number: Int,
     @ColumnInfo(name = "image_url") val imageUrl: String,
