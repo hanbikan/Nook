0f093b1452a9fa5497103c51f1afb27743665457
hanbikan <fchopinof99@gmail.com>
2024-03-30 16:42:40 +0900

    Implement RemoteCollectionTranslator

--- core/network/src/main/java/com/hanbikan/nook/core/network/translator/RemoteCollectionTranslator.kt
+++ core/network/src/main/java/com/hanbikan/nook/core/network/translator/RemoteCollectionTranslator.kt
@@ -1,1 +1,19 @@
 package com.hanbikan.nook.core.network.translator
+
+import com.hanbikan.nook.core.domain.model.Fish
+import com.hanbikan.nook.core.domain.response.FishResponse
+import com.hanbikan.nook.core.domain.response.toMap
+
+fun FishResponse.toDomain(
+    userId: Int,
+    isNorth: Boolean, // 북반구 여부
+): Fish {
+    return Fish(
+        userId = userId,
+        name = name,
+        number = number,
+        imageUrl = image_url,
+        timesByMonth = if (isNorth) north.times_by_month.toMap() else south.times_by_month.toMap(),
+        isCollected = false
+    )
+}
