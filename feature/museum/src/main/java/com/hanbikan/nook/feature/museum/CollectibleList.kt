7f20b7f44924c6fd4a947ac0f9d726d3f9abd945
hanbikan <fchopinof99@gmail.com>
2024-05-21 17:04:06 +0900

    Add check icon to represent isCollected

--- feature/museum/src/main/java/com/hanbikan/nook/feature/museum/CollectibleList.kt
+++ feature/museum/src/main/java/com/hanbikan/nook/feature/museum/CollectibleList.kt
@@ -1,12 +1,16 @@
 package com.hanbikan.nook.feature.museum
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.hanbikan.nook.core.designsystem.component.NkText
+import com.hanbikan.nook.core.designsystem.theme.NkTheme
 import com.hanbikan.nook.core.domain.model.Collectible
 
 private val CollectibleItemSize = 80.dp
@@ -39,17 +44,30 @@ fun CollectibleItem(
     item: Collectible,
     onClick: () -> Unit,
 ) {
-    Column(
+    Box(
         modifier = Modifier
             .size(CollectibleItemSize)
-            .clickable { onClick() },
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .clickable { onClick() }
     ) {
-        GlideImage(
-            modifier = Modifier.size(CollectibleItemSize * 0.5f),
-            model = item.imageUrl,
-            contentDescription = item.name,
-        )
-        NkText(text = item.name)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            GlideImage(
+                modifier = Modifier.size(CollectibleItemSize * 0.5f),
+                model = item.imageUrl,
+                contentDescription = item.name,
+            )
+            NkText(text = item.name)
+        }
+
+        if (item.isCollected) {
+            Icon(
+                modifier = Modifier.align(Alignment.TopEnd),
+                imageVector = Icons.Default.Check,
+                contentDescription = null,
+                tint = NkTheme.colorScheme.secondary,
+            )
+        }
     }
 }
