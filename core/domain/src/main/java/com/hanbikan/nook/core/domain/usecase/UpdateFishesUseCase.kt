526ba7926d54ad52a9d24631caba0a85829990c9
hanbikan <fchopinof99@gmail.com>
2024-05-30 17:43:31 +0900

    Update UpdateFishesUseCase to set isCollected

--- core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/UpdateFishesUseCase.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/UpdateFishesUseCase.kt
@@ -2,6 +2,7 @@ package com.hanbikan.nook.core.domain.usecase
 
 import com.hanbikan.nook.core.domain.repository.CollectionRepository
 import com.hanbikan.nook.core.domain.repository.RemoteCollectionRepository
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class UpdateFishesUseCase @Inject constructor(
@@ -9,11 +10,18 @@ class UpdateFishesUseCase @Inject constructor(
     private val collectionRepository: CollectionRepository,
 ) {
     suspend operator fun invoke(userId: Int) {
+        val collectedNames = collectionRepository.getAllFishesByUserId(userId).first()
+            .filter { it.isCollected }
+            .map { it.name }
+            .toSet()
+        collectionRepository.deleteAllFishesByUserId(userId)
+
         val remoteFishes = remoteCollectionRepository.getAllFishes(
             userId = userId,
             isNorth = true, // TODO: 북반구/남반구 정보 반영
-        )
-        collectionRepository.deleteAllFishes(userId)
+        ).map {
+            it.copy(isCollected = it.name in collectedNames)
+        }
         collectionRepository.insertFishes(remoteFishes)
     }
 }
