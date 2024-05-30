c859d0bd4848586d935d4bd1728aee9f6ec3b507
hanbikan <fchopinof99@gmail.com>
2024-05-30 17:41:33 +0900

    Add BugEntity

---
+++ core/database/src/main/java/com/hanbikan/nook/core/database/entity/BugEntity.kt
@@ -0,0 +1,17 @@
+package com.hanbikan.nook.core.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bug")
+data class BugEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "user_id") val userId: Int,
+    val name: String,
+    val number: Int,
+    @ColumnInfo(name = "image_url") val imageUrl: String,
+    @ColumnInfo(name = "times_by_month") val timesByMonth: Map<Int, String>,
+    val isCollected: Boolean,
+    val location: String,
+)
