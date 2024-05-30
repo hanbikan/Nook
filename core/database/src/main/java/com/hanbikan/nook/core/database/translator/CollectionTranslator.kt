a166160bc7d5c600ec7e4e96190bed800a321886
hanbikan <fchopinof99@gmail.com>
2024-05-30 17:42:40 +0900

    Implement bug translation

--- core/database/src/main/java/com/hanbikan/nook/core/database/translator/CollectionTranslator.kt
+++ core/database/src/main/java/com/hanbikan/nook/core/database/translator/CollectionTranslator.kt
@@ -1,6 +1,8 @@
 package com.hanbikan.nook.core.database.translator
 
+import com.hanbikan.nook.core.database.entity.BugEntity
 import com.hanbikan.nook.core.database.entity.FishEntity
+import com.hanbikan.nook.core.domain.model.Bug
 import com.hanbikan.nook.core.domain.model.Fish
 
 fun FishEntity.toDomain(): Fish {
@@ -18,6 +20,32 @@ fun FishEntity.toDomain(): Fish {
 
 fun Fish.toData(): FishEntity {
     return FishEntity(
+        id = id,
+        userId = userId,
+        name = name,
+        number = number,
+        imageUrl = imageUrl,
+        timesByMonth = timesByMonth,
+        isCollected = isCollected,
+        location = location,
+    )
+}
+
+fun BugEntity.toDomain(): Bug {
+    return Bug(
+        id = id,
+        userId = userId,
+        name = name,
+        number = number,
+        imageUrl = imageUrl,
+        timesByMonth = timesByMonth,
+        isCollected = isCollected,
+        location = location
+    )
+}
+
+fun Bug.toData(): BugEntity {
+    return BugEntity(
         id = id,
         userId = userId,
         name = name,
