7aac0e8e2a804d5d367d1a1a2d3ec727ca9709e3
hanbikan <fchopinof99@gmail.com>
2024-05-30 17:43:03 +0900

    Implement collection repository for bug

--- core/database/src/main/java/com/hanbikan/nook/core/database/repository/CollectionRepositoryImpl.kt
+++ core/database/src/main/java/com/hanbikan/nook/core/database/repository/CollectionRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.hanbikan.nook.core.database.repository
 import com.hanbikan.nook.core.database.dao.CollectionDao
 import com.hanbikan.nook.core.database.translator.toData
 import com.hanbikan.nook.core.database.translator.toDomain
+import com.hanbikan.nook.core.domain.model.Bug
 import com.hanbikan.nook.core.domain.model.Fish
 import com.hanbikan.nook.core.domain.repository.CollectionRepository
 import kotlinx.coroutines.flow.Flow
@@ -12,6 +13,7 @@ import javax.inject.Inject
 class CollectionRepositoryImpl @Inject constructor(
     private val collectionDao: CollectionDao,
 ) : CollectionRepository {
+    // Fish
     override fun getAllFishesByUserId(userId: Int): Flow<List<Fish>> {
         return collectionDao.getAllFishesByUserId(userId).map { fishes ->
             fishes.map { it.toDomain() }
@@ -26,7 +28,26 @@ class CollectionRepositoryImpl @Inject constructor(
         collectionDao.updateFish(fish.toData())
     }
 
-    override suspend fun deleteAllFishes(userId: Int) {
+    override suspend fun deleteAllFishesByUserId(userId: Int) {
         collectionDao.deleteAllFishes(userId)
+    }
+
+    // Bug
+    override fun getAllBugsByUserId(userId: Int): Flow<List<Bug>> {
+        return collectionDao.getAllBugsByUserId(userId).map { bugs ->
+            bugs.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun insertBugs(bugList: List<Bug>) {
+        collectionDao.insertBugs(*bugList.map { it.toData() }.toTypedArray())
+    }
+
+    override suspend fun updateBug(bug: Bug) {
+        collectionDao.updateBug(bug.toData())
+    }
+
+    override suspend fun deleteAllBugsByUserId(userId: Int) {
+        collectionDao.deleteAllBugs(userId)
     }
 }
--- core/domain/src/main/java/com/hanbikan/nook/core/domain/repository/CollectionRepository.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/repository/CollectionRepository.kt
@@ -1,14 +1,25 @@
 package com.hanbikan.nook.core.domain.repository
 
+import com.hanbikan.nook.core.domain.model.Bug
 import com.hanbikan.nook.core.domain.model.Fish
 import kotlinx.coroutines.flow.Flow
 
 interface CollectionRepository {
+    // Fish
     fun getAllFishesByUserId(userId: Int): Flow<List<Fish>>
 
     suspend fun insertFishes(fishList: List<Fish>)
 
     suspend fun updateFish(fish: Fish)
 
-    suspend fun deleteAllFishes(userId: Int)
+    suspend fun deleteAllFishesByUserId(userId: Int)
+
+    // Bug
+    fun getAllBugsByUserId(userId: Int): Flow<List<Bug>>
+
+    suspend fun insertBugs(bugList: List<Bug>)
+
+    suspend fun updateBug(bug: Bug)
+
+    suspend fun deleteAllBugsByUserId(userId: Int)
 }
