9ada9320a33baaeda4666d16870714ba50e31459
hanbikan <fchopinof99@gmail.com>
2024-05-31 16:02:25 +0900

    Modify UpdateUserUseCase

--- core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/UpdateUserUseCase.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/usecase/UpdateUserUseCase.kt
@@ -5,9 +5,11 @@ import javax.inject.Inject
 class UpdateUserUseCase @Inject constructor(
     private val updateFishesUseCase: UpdateFishesUseCase,
     private val updateBugsUseCase: UpdateBugsUseCase,
+    private val updateSeaCreaturesUseCase: UpdateSeaCreaturesUseCase,
 ) {
     suspend operator fun invoke(userId: Int) {
         updateFishesUseCase(userId)
         updateBugsUseCase(userId)
+        updateSeaCreaturesUseCase(userId)
     }
 }
