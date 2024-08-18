aafe17ff1169c6a9b81e307f95a488b93560abc2
hanbikan <fchopinof99@gmail.com>
2024-08-18 21:34:16 +0900

    Add shadow size translator

--- core/domain/src/main/java/com/hanbikan/nook/core/domain/util/ShadowTranslator.kt
+++ core/domain/src/main/java/com/hanbikan/nook/core/domain/util/ShadowTranslator.kt
@@ -2,12 +2,16 @@ package com.hanbikan.nook.core.domain.util
 
 val shadowSizeToKorean: Map<String, String> = mapOf(
     "X-Small" to "작음",
+    "Tiny" to "작음",
     "Small" to "약간 작음",
     "Medium" to "중간",
     "Medium w/Fin" to "중간(등지느러미)",
     "Large" to "약간 큼",
     "Large w/Fin" to "약간 큼(등지느러미)",
+    "Very large" to "큼",
+    "Very large (finned)" to "큼(등지느러미)",
     "X-Large" to "큼",
     "XX-Large" to "매우 큼",
+    "Huge" to "매우 큼",
     "Long" to "길음",
 )
