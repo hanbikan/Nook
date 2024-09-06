13f5a4d87a25b6b69162cdd0b969c93682d77986
hanbikan <fchopinof99@gmail.com>
2024-09-07 00:25:33 +0900

    Update version to 1.5.0(9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hanbikan.nook"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.4.0"
+        versionCode = 9
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
